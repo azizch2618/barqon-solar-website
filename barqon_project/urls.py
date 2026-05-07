d553f064628b277b5ea772f514ae930a7c13d8de
Aziz Ullah <azizch19999@gmail.com>
2026-05-07 20:46:35 +0500

    Fix media URLs properly

--- barqon_project/urls.py
+++ barqon_project/urls.py
@@ -1,11 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core.views import home
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("", home, name="home"),
@@ -23,3 +22,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Serving media and static in production
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
