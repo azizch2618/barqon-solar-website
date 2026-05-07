2bab4cec475ab7a3860a60defc0ea70dbc6ad4cb
Aziz Ullah <azizch19999@gmail.com>
2026-05-07 21:44:12 +0500

    Fixed Railway static images

--- barqon_project/settings.py
+++ barqon_project/settings.py
@@ -90,6 +90,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
